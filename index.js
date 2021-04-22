574030e7eb67a6596702ad7e1c789f7455d97195
Robert Taveras <monkeyeden@Roberts-MBP.lan>
2021-04-22 17:48:08 -0400

    Task 1

--- index.js
+++ index.js
@@ -42,7 +42,22 @@ class Airplane {
   */
   
  class Person {
-    
+   constructor(name,age){
+     this.name = name;
+     this.age = age;
+     this.stomach = [];
+   }
+   eat(edible){
+     if(this.stomach.length < 10){
+       this.stomach.push(edible)
+     }
+   }
+   poop(){
+     this.stomach = [];
+   }
+   toString(){
+     return `${this.name}, ${this.age}`
+   }
   }
   
   /*
@@ -60,7 +75,23 @@ class Airplane {
   */
   
  class Car {
-    
+   constructor(model,milesPerGallon){
+     this.model = model;
+     this.milesPerGallon = milesPerGallon;
+     this.tank = 0;
+     this.odometer = 0;
+   }
+   fill(gallons){
+      this.tank += gallons;
+   }
+    drive(distance){
+      this.odometer += distance;
+      this.tank -= this.milesPerGallon;
+      // return `I ran out of fuel at ${this.odometer} miles!`
+
+
+
+    }
   }
   
   /*
